a29d0790e5b6100cda2882e761217af21e8b6b06
Jhern570 <juliohdezjrn@gmail.com>
2025-10-08 21:44:05 -0700

    First commit

---
+++ graph.c
@@ -0,0 +1,111 @@
+#include "graph.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+void insert_link_between_two_nodes(node_t *node1, node_t* node2, char *from_if_name, char *to_if_name, unsigned int cost){
+
+	link_t* link = calloc(1, sizeof(link_t));
+
+	strncpy(link->intf1.if_name, from_if_name, IF_NAME_SIZE);
+	strncpy(link->intf2.if_name, to_if_name, IF_NAME_SIZE);
+
+	link->intf1.link = link; //setback pointer to link
+	link->intf2.link = link; //setback pointer to link
+
+	link->intf1.att_node = node1;
+	link->intf2.att_node = node2;
+
+	link->cost = cost;
+
+	int empty_intf_slot;
+
+	//plugin interface ends into Node
+	empty_intf_slot = get_node_intf_available_slot(node1);
+	node1->intf[empty_intf_slot] = &link->intf1;
+
+	empty_intf_slot = get_node_intf_available_slot(node2);
+        node2->intf[empty_intf_slot] = &link->intf2;
+
+}
+
+
+
+graph_t* create_new_graph(char *topology_name){
+
+	graph_t* graph = calloc(1, sizeof(graph_t));
+	strncpy(graph->topology_name, topology_name, 32);
+	graph->topology_name[31] = '\0';
+	
+	init_glthread(&graph->node_list);
+	return graph;
+	
+}
+
+node_t* create_graph_node(graph_t * graph, char *node_name){
+
+        node_t* node = calloc(1, sizeof(node_t));
+        strncpy(node->node_name, node_name, NODE_NAME_SIZE);
+        node->node_name[31] = '\0';
+ 
+        init_glthread(&node->graph_glue);
+        glthread_add_next(&graph->node_list, &node->graph_glue);
+	return node;
+ 
+}
+
+graph_t* build_first_topo(){
+
+
+	graph_t * topo = create_new_graph("Hello World Generic Graph");
+	node_t* R0_re = create_graph_node(topo, "R0_re");
+	node_t* R1_re = create_graph_node(topo, "R1_re");
+	node_t* R2_re = create_graph_node(topo, "R2_re");
+
+	insert_link_between_two_nodes(R0_re, R1_re, "eth0/0", "eth0/1", 1);
+	insert_link_between_two_nodes(R1_re, R2_re, "eth0/2", "eth0/3", 1);
+	insert_link_between_two_nodes(R0_re, R2_re, "eth0/4", "eth0/5", 1);
+
+	return topo;
+
+}
+
+void dump_graph(graph_t *graph){
+
+    node_t *node;
+    glthread_t *curr;
+
+    printf("Topology Name = %s\n", graph->topology_name);
+
+    ITERATE_GLTHREAD_BEGIN(&graph->node_list, curr){
+
+        node = graph_glue_to_node(curr);
+        dump_node(node);
+    } ITERATE_GLTHREAD_END(&graph->node_list, curr);
+}
+
+void dump_node(node_t *node){
+
+    unsigned int i = 0;
+    interface_t *intf;
+
+    printf("Node Name = %s : \n", node->node_name);
+    for( ; i < MAX_INTF_PER_NODE; i++){
+
+        intf = node->intf[i];
+        if(!intf) break;
+        dump_interface(intf);
+    }
+}
+
+void dump_interface(interface_t *interface){
+
+   link_t *link = interface->link;
+   node_t *nbr_node = get_nbr_node(interface);
+
+   printf("Interface Name = %s\n\tNbr Node %s, Local Node : %s, cost = %u\n",
+            interface->if_name,
+            nbr_node->node_name,
+            interface->att_node->node_name,
+            link->cost);
+}
---
+++ graph.h
@@ -0,0 +1,107 @@
+#include <assert.h>
+#include "gluethread/glthread.h"
+
+
+
+#define NODE_NAME_SIZE		16
+#define IF_NAME_SIZE		16
+#define MAX_INTF_PER_NODE	10
+
+
+//Forward Declaration
+typedef struct node_ node_t;
+typedef struct link_ link_t;
+
+typedef struct interface{
+	
+	char if_name[IF_NAME_SIZE];
+	struct node_ *att_node;
+	struct link_ *link;
+}interface_t;
+
+struct link_{
+	interface_t intf1;
+	interface_t intf2;
+	unsigned int cost;
+};
+
+struct node_{
+	char node_name[NODE_NAME_SIZE];
+	interface_t *intf[MAX_INTF_PER_NODE];
+	glthread_t graph_glue;
+};
+
+typedef struct graph{
+
+	char topology_name[32];
+	glthread_t node_list;
+} graph_t;
+
+GLTHREAD_TO_STRUCT(graph_glue_to_node, node_t, graph_glue);
+
+//Helper functions
+static inline node_t* get_nbr_node(interface_t*interface){
+
+	assert(interface->att_node);
+	assert(interface->link);
+
+	link_t* link = interface->link;
+
+	if(&link->intf1 == interface){
+		return link->intf2.att_node;
+	}else{
+		return link->intf1.att_node;
+	}	
+
+}
+
+static inline int get_node_intf_available_slot(node_t* node){
+	for(int i = 0; i < MAX_INTF_PER_NODE; i++){
+		if(node->intf[i]){
+			continue;
+		}
+		return i;
+	}
+	return -1;
+}
+
+static inline interface_t * get_node_if_by_name(node_t* node, char* if_name){
+
+	interface_t* intf;
+
+	for(int i = 0; i < MAX_INFT_PER_NODE; i++){
+	
+		intf = node->intf[i];
+		if(!intf) return NULL;
+
+		if(strncmp(intf[i]->if_name, if_name, IF_NAME_SIZE) == 0){
+			return node->intf[i];
+		}
+
+	}
+
+	return NULL;
+	 
+}
+
+static line node_t* get_node_by_node_name(graph_t *topo, char* node_name){
+
+	node_t* node;
+	glthread_t *curr;
+
+	ITERATE_GLTHREAD_BEGIN(&topo->node_list, curr){
+	
+		node = graph_glue_to_node(curr);
+	
+		if(strncmp(node->node_name, node_name, strlen(node_name)) == 0){
+		       return node;
+		} 
+	} ITERATE_GLTHREAD_END(&topo->node_list, curr);
+	return NULL;
+}
+
+void dump_graph(graph_t* graph);
+
+void dump_node(node_t* node);
+
+void dump_interface(interface_t* interface);
